89fc1eb7a409404b145e8d10f4167b9b3699f10a
Alejandro Aranda <alexwing@gmail.com.com>
2020-06-26 18:51:55 +0200

    White flash shader

---
+++ Assets/Scripts/WhiteFlash.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteFlash : MonoBehaviour
+{
+
+    public float intensity;
+    private Material material;
+
+    // Creates a private material used to the effect
+    void Awake()
+    {
+        material = new Material(Shader.Find("Custom/WhiteFlash"));
+    }
+
+    // Postprocess the image
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (intensity == 0)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        material.SetFloat("_flash", intensity);
+        Graphics.Blit(source, destination, material);
+    }
+}
